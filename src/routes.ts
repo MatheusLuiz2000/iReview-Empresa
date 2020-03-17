a1eb52dbea5607e46a0fd623a2f53a718022c237
Thiago Ribeiro <macpro@MacBook-Pro-de-Macbook.local>
2020-03-17 15:13:13 -0300

    Alterado rotas

--- src/routes.ts
+++ src/routes.ts
@@ -6,11 +6,12 @@ import TedValidator from './app/validators/TedValidator';
 const routes = new Router();
 
 routes.get('/', TedController.docs); // Rota para a documentação
-routes.get('/teds', TedController.listar); // Rota para a documentação
-routes.get('/teds/:id', TedValidator.buscaTedById, TedController.listarById); // Rota para a documentação
-routes.post('/teds/cadastrar/', TedValidator.criaTed, TedController.criarTed);
-routes.post('/teds/gerar/', TedController.gerarTed);
-routes.post('/teds/ler/retorno', TedController.leituraRetornoTed);
+routes.get('/historico', TedController.listar); // Rota para a documentação
+routes.get('/:id', TedValidator.buscaTedById, TedController.listarById); // Rota para a documentação
+routes.post('/cadastrar/', TedValidator.criaTed, TedController.criarTed);
+routes.post('/gerar/', TedController.gerarTed);
+routes.post('/retorno', TedController.leituraRetornoTed);
+
 routes.get('/health', TedController.health); // Rota para o health check
 routes.get('/testes', TedController.testes); // Rota para o health check
 
